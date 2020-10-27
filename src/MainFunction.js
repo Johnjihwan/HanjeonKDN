33c946319e751290f08c68d3b193ca2ca9fe4e8e
joenjihwan <s20062@gsm.hs.kr>
2020-10-27 12:26:10 +0900

    login location 설정했습니다.

--- src/MainFunction.js
+++ src/MainFunction.js
@@ -1,3 +1,9 @@
 $('.login').click(function(){
-    alert();
+    let YesOrNo = confirm("로그인 페이지로 이동할까요?");
+    if(YesOrNo == true){
+        location.href = 'signup_page.html';
+    }
+    else{
+        location.reload();
+    }
 })
