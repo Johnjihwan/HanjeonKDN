2a3eec798b5cb5c7a70220845ab879d4e955f438
joenjihwan <s20062@gsm.hs.kr>
2020-11-01 02:44:53 +0900

    약간의 모순이 있지만 그래도 지장은 없다

--- src/MainFunction.js
+++ src/MainFunction.js
@@ -1,12 +1,16 @@
+// 메인페이지 load 기본상태는 팝업 display:none
+let LayerPopup = $(".layer-popup");
+LayerPopup.removeClass("show");
+
 // 팝업 열기
 $(document).on("click", ".btn-open", function (e){
-    var target = $(this).attr("href");
+    let target = $(this).attr("href");
     $(target).addClass("show");
 });
 
 // 외부영역 클릭 시 팝업 닫기
 $(document).mouseup(function (e){
-    var LayerPopup = $(".layer-popup");
+    LayerPopup = $(".layer-popup");
     if(LayerPopup.has(e.target).length === 0){
         LayerPopup.removeClass("show");
     }
